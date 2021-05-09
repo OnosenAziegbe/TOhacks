85939196c051a04d26e46f036d94b2a8d8a6ef49
OnosenAziegbe <59236207+OnosenAziegbe@users.noreply.github.com>
2021-05-09 13:02:37 -0400

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,26 @@
+var friends = [
+	{
+		name: "Charlie",
+		scores: [0, 1, 0, 1, 0, 1]
+	},
+	{
+		name: "Sweet Dee",
+		scores: [0, 0, 0, 0, 0, 0]
+	},
+	{
+		name: "Dennis",
+		scores: [1, 1, 1, 1, 1, 1]
+	},
+	{
+		name: "Mac",
+		scores: [1, 0, 1, 0, 1, 0]
+	},
+	{
+		name: "Frank",
+		scores: [0, 0, 1, 1, 0, 1]
+
+	}
+
+];
+
+module.exports = friends;
